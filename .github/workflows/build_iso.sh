1d9b4077906549834db7c62c5e928431f84be0a3
zamkara <zamkara@gnuweeb.org>
2026-05-31 00:19:33 +0700

    refactor: simplify ISO build script by utilizing AUR packages

--- .github/workflows/build_iso.sh
+++ .github/workflows/build_iso.sh
@@ -9,16 +9,7 @@ mkdir -p /work/.github/workflows/archiso/custom_repo
 cp /work/aur-packages/*.pkg.tar.zst /work/.github/workflows/archiso/custom_repo/
 repo-add /work/.github/workflows/archiso/custom_repo/custom.db.tar.gz /work/.github/workflows/archiso/custom_repo/*.pkg.tar.zst
 
-# Copy Alga Binary into Live ISO
-mkdir -p /work/.github/workflows/archiso/airootfs/usr/bin
-cp /work/alga-binary/alga /work/.github/workflows/archiso/airootfs/usr/bin/alga
-chmod +x /work/.github/workflows/archiso/airootfs/usr/bin/alga
-
-echo "==> Downloading MoreWaita icon theme..."
-mkdir -p /work/.github/workflows/archiso/airootfs/usr/share/icons
-git clone https://github.com/somepaulo/MoreWaita.git /tmp/MoreWaita
-cp -r /tmp/MoreWaita/* /work/.github/workflows/archiso/airootfs/usr/share/icons/MoreWaita/ 2>/dev/null || cp -r /tmp/MoreWaita /work/.github/workflows/archiso/airootfs/usr/share/icons/
-rm -rf /tmp/MoreWaita
+# ISO rootfs is populated by pacman packages from custom_repo
 
 # Run mkarchiso
 sed -i 's/airootfs\.sfs/originium.sfs/g' /usr/bin/mkarchiso
